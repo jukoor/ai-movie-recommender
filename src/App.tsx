7a80fd92469f98478b82642b22773984a6c94dc7
Julian Orth <julian.orth@icloud.com>
2025-07-14 12:58:54 +0200

    Cleanup files after initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <title>AI Movie Recommender</title>
+      <h1>🤖 AI Movie Recommender</h1>
+    </div>
+  );
+}
+
+export default App;
